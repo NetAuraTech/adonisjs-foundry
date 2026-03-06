29ab8ddcd5ee86e9b97f829117bc412316a854ea
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2026-03-06 22:28:56 +0100

    feat(env)
    Add types for env values

--- start/env.ts
+++ start/env.ts
@@ -12,29 +12,70 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  // Node
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring node
+  |----------------------------------------------------------
+  */
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
-  // App
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring app
+  |----------------------------------------------------------
+  */
   APP_KEY: Env.schema.secret(),
   APP_URL: Env.schema.string({ format: 'url', tld: false }),
+  APP_NAME: Env.schema.string(),
 
-  // Session
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring session
+  |----------------------------------------------------------
+  */
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database', 'redis'] as const),
 
-  // Database
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring database connection
+  |----------------------------------------------------------
+  */
   PG_HOST: Env.schema.string({ format: 'host' }),
   PG_PORT: Env.schema.number(),
   PG_USER: Env.schema.string(),
   PG_PASSWORD: Env.schema.secret(),
   PG_DB_NAME: Env.schema.string(),
 
-  // Redis
+  /*
+  |--------------------------------------------------------------------------
+  | Variables for configuring @adonisjs/redis package
+  |--------------------------------------------------------------------------
+  */
   REDIS_HOST: Env.schema.string({ format: 'host' }),
   REDIS_PORT: Env.schema.number(),
   REDIS_PASSWORD: Env.schema.string.optional(),
   REDIS_SOCKET: Env.schema.string.optional(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the mail package
+  |----------------------------------------------------------
+  */
+  MAIL_MAILER: Env.schema.enum(['smtp'] as const),
+  MAIL_FROM_NAME: Env.schema.string(),
+  MAIL_FROM_ADDRESS: Env.schema.string(),
+  SMTP_HOST: Env.schema.string(),
+  SMTP_PORT: Env.schema.number(),
+  SMTP_USERNAME: Env.schema.string(),
+  SMTP_PASSWORD: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring @rlanz/sentry package
+  |----------------------------------------------------------
+  */
+  SENTRY_DSN: Env.schema.string(),
 })
