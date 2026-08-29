dea9dfb339965b17076ac50b0f57dbf4ffc61d78
Lhomme Aymeric <aymericlhomme@orange.fr>
2026-08-29 20:50:32 +0200

    chore(prune): sync @aws-sdk/client-s3 range in flavor manifests to 3.1117.0

--- tooling/prune/flavors/inertia.manifest.ts
+++ tooling/prune/flavors/inertia.manifest.ts
@@ -1295,7 +1295,7 @@ const inertiaManifest: FlavorManifest = {
 				'    "@adonisjs/transmit": "^3.0.1",',
 				'    "@adonisjs/transmit-client": "^1.1.0",',
 				'    "@adonisjs/vite": "^6.0.0",',
-				'    "@aws-sdk/client-s3": "^3.1102.0",',
+				'    "@aws-sdk/client-s3": "^3.1117.0",',
 				'    "@aws-sdk/s3-request-presigner": "^3.1117.0",',
 				'    "@fontsource/cormorant-garamond": "^5.2.11",',
 				'    "@fontsource/jost": "^5.3.0",',
